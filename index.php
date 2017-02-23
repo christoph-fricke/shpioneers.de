ea900db30da92093e2b0d9d66309b4e1cd06d4e3
Christoph Fricke <christoph@frickeonline.de>
2017-02-24 00:35:38 +0100

    Added index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    include('assets/php/checkLanguage.php');
+?>
+
+<!DOCTYPE html>
+<html lang="de-de">
+
+<head>
+  <title><?php echo TITLE ?></title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" href="assets/img/favicon.ico" />
+
+  <meta name="author" content="Christoph Fricke" />
+
+  <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+
+<body>
+  <script src="assets/js/functions.js"></script>
+</body>
+
+</html>
