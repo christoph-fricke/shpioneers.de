ef9fefd7d974f071fdb45e642d7cb10ba7fa6250
Christoph Fricke <christoph@frickeonline.de>
2017-02-24 01:07:11 +0100

    Start to add Navbar

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
 <?php
-    include('assets/php/checkLanguage.php');
+include('assets/php/checkLanguage.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="de-de">
+  <!DOCTYPE html>
+  <html lang="de-de">
 
-<head>
-  <title><?php echo TITLE ?></title>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="icon" href="assets/img/favicon.ico" />
+  <head>
+    <title>
+      <?php echo TITLE ?>
+    </title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="assets/img/favicon.ico" />
 
-  <meta name="author" content="Christoph Fricke" />
+    <meta name="author" content="Christoph Fricke" />
 
-  <link rel="stylesheet" href="assets/css/style.css" />
-</head>
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
 
-<body>
-  <script src="assets/js/functions.js"></script>
-</body>
+  <body>
+    <nav>
+      <ul>
+        <li>
+        </li>
+      </ul>
+    </nav>
 
-</html>
+    <script src="assets/js/functions.js"></script>
+  </body>
+
+  </html>
