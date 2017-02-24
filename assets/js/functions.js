d33abf4f956016d3f62f989cc8fdd9f763407909
quannten24 <bendix.sonnenberg@gmx.de>
2017-02-24 13:11:19 +0100

    actually made the scrollling to an anchor work // !=  \\

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,4 @@
-$('a[href*=#]').on('click', function(event){     
+$(document).ready(function(){$("a[href*=\\#]").click(function(event){     
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-});
+});});
--- index.php
+++ index.php
@@ -14,20 +14,19 @@ include('assets/php/checkLanguage.php');
     <link rel="icon" href="assets/img/favicon.ico" />
 
     <meta name="author" content="Christoph Fricke" />
-
     <link rel="stylesheet" href="assets/css/style.css" />
-    
   </head>
 
   <body>
     <nav>
       <ul>
         <li>
+        <a href="#down">pioneers</a>
         </li>
       </ul>
     </nav>
-    <script src ="https://code.jquery.com/jquery-3.1.1.min.js">>/script>
+    <div id="down">hier steht text</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>    
     <script src="assets/js/functions.js"></script>
-    
   </body>
   </html>
