8c9619c68a2fca579c0f28f7e03d2dbf06ba6899
Christoph Fricke <christoph@frickeonline.de>
2017-02-24 23:37:20 +0100

    Added assets for dropdown menue

--- assets/lang/en-en.php
+++ assets/lang/en-en.php
@@ -3,10 +3,12 @@
 define('TITLE', 'Pioneers');
 
 //Nav content
-define('NEWS', "News");
+define('NEWS', 'News');
 define('CONTEST', 'Contest');
 define('SPONSORS', 'Sponsors');
 define('SUCESS', 'Sucesses');
 define('TEAM', 'Team');
 define('LANGUAGE', 'English');
+define('GERMAN', 'German');
+define('ENGLISH', 'English')
 ?>
