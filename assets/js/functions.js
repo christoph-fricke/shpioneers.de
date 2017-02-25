0537592c31917a8e526716b00d265342251637f3
Christoph Fricke <christoph@frickeonline.de>
2017-02-25 18:14:58 +0100

    Made scrolling respecting the nav bar

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){$("a[href*=\\#]").click(function(event){     
     event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    $('html,body').animate({scrollTop:$(this.hash).offset().top - 70}, 500);
 });});
