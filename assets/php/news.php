2695b955df5891277ac8bf4f8336d5580654da63
quannten24 <bendix.sonnenberg@gmx.de>
2017-02-25 14:48:12 +0100

    added link to full newspage

--- assets/php/news.php
+++ assets/php/news.php
@@ -1,9 +1,9 @@
 <?php
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // this was debugging although the php doesnt show syntx errors
-//writenews(1); calling the function to see wether it does what it promises
+writenews(1); //calling the function to see wether it does what it promises
 function writenews(int $index){ // the index describes the number to which news number news is supposed to be displayed
 session_start();
 if(!isset($_SESSION['lang'])){
@@ -25,7 +25,7 @@ echo '<div class="news-title">'. $thisnews ->  title. "</div>";
 echo '</div>';
 echo '<div class="news-bottom">';
 echo '<div class="news-prev">'. $thisnews ->  preview. "</div>";
-echo '<div class="news-button"> MEHR ERFAHREN</div>';
+echo '<a class="news-button" href="newss.php?ind='.$i.'"> MEHR ERFAHREN</a>';
 echo '</div></div>';
 // output, well i could write all this in one line but that would be unreadable so i dont
 if($i == $index){ // manual for loop because during development i was caugt up in foreach and i am to lazy to switch it
