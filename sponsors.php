c389a7882e35b7ac3aa28ad50e946489b0905806
quannten24 <bendix.sonnenberg@gmx.de>
2017-03-13 17:55:48 +0100

    small changes

--- sponsors.php
+++ sponsors.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
 if(!isset($_SESSION['lang'])) $_SESSION['lang'] = "de-de";
-var_dump(getdata('finance'));
+var_dump(getdata($_GET['type']));
 function getdata($type){
+if(!file_exists("content/sponsors/". $type ."-". $_SESSION['lang']. ".json")) die();
 return json_decode(file_get_contents("content/sponsors/". $type ."-". $_SESSION['lang']. ".json"));
 }
 ?>
