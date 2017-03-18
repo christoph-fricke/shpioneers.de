df6c6a0fd2b3d7e952a8b532daacc3078eab1127
quannten24 <bendix.sonnenberg@gmx.de>
2017-03-18 09:20:42 +0100

    is now included in the sponsors/index.php file

--- sponsors.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!isset($_SESSION['lang'])) $_SESSION['lang'] = "de-de";
-function getdata($type){
-if(!file_exists("../content/sponsors/". $type ."-". $_SESSION['lang']. ".json")) die();
-return json_decode(file_get_contents("content/sponsors/". $type ."-". $_SESSION['lang']. ".json"));
-}
-?>
