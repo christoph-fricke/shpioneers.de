2f3aca80e192739f7e63f9c9a152e44f4e748ad8
quannten24 <bendix.sonnenberg@gmx.de>
2017-03-18 17:42:04 +0100

    if an session is already establish the language can be used

--- assets/php/checkLanguage.php
+++ assets/php/checkLanguage.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-
+if(!isset($_SESSION['lang']){
 if (isset($_GET['lang'])) {
     $_SESSION['lang'] = $_GET['lang'];
 }
 else {
     $_SESSION['lang'] = "de-de";
-}
+}}
 
 switch ($_SESSION['lang']) {
     case "en-en":
