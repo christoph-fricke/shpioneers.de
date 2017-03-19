8ddbcfda5c2c4e23fc4d96d7301a56d74d125d92
quannten24 <bendix.sonnenberg@gmx.de>
2017-03-19 22:09:57 +0100

    trophies are generated

--- achievments/index.php
+++ achievments/index.php
@@ -27,6 +27,12 @@ function setHtmlLang() {
         echo "de-de";
     }
 }
+function printsuccesses(){
+global $tournament;
+foreach($tournament[0] -> trophies as $su){
+	printsuccess($su);
+}
+}
 function printsuccess($suc){
 echo '        <div class="achievment-content">
           <div class="trophy achievment-trophy">
@@ -36,14 +42,7 @@ echo '        <div class="achievment-content">
             </svg>
           </div>
           <h2>'. $suc -> name.'</h2>
-        </div>
-        <div class="achievment-content">
-          <div class="trophy achievment-trophy">
-            <svg class="icon-big" viewBox="0 0 24 24">
-              <path d="M20.2,2H19.5H18C17.1,2 16,3 16,4H8C8,3 6.9,2 6,2H4.5H3.8H2V11C2,12 3,13 4,13H6.2C6.6,15 7.9,16.7 11,17V19.1C8.8,19.3 8,20.4 8,21.7V22H16V21.7C16,20.4 15.2,19.3 13,19.1V17C16.1,16.7 17.4,15 17.8,13H20C21,13 22,12 22,11V2H20.2M4,11V4H6V6V11C5.1,11 4.3,11 4,11M20,11C19.7,11 18.9,11 18,11V6V4H20V11Z"
-              />
-            </svg>
-          </div>'
+        </div>';
 }
 ?>
 
@@ -138,28 +137,8 @@ echo '        <div class="achievment-content">
 
     <section class="contest">
       <main class="inner-contest achievment-box">
-
-          <h2>Coolest portfolie</h2>
-        </div>
-        <div class="achievment-content">
-          <div class="trophy achievment-trophy">
-            <svg class="icon-big" viewBox="0 0 24 24">
-              <path d="M20.2,2H19.5H18C17.1,2 16,3 16,4H8C8,3 6.9,2 6,2H4.5H3.8H2V11C2,12 3,13 4,13H6.2C6.6,15 7.9,16.7 11,17V19.1C8.8,19.3 8,20.4 8,21.7V22H16V21.7C16,20.4 15.2,19.3 13,19.1V17C16.1,16.7 17.4,15 17.8,13H20C21,13 22,12 22,11V2H20.2M4,11V4H6V6V11C5.1,11 4.3,11 4,11M20,11C19.7,11 18.9,11 18,11V6V4H20V11Z"
-              />
-            </svg>
-          </div>
-          <h2>Best messestand</h2>
-        </div>
-        <div class="achievment-content">
-          <div class="trophy achievment-trophy">
-            <svg class="icon-big" viewBox="0 0 24 24">
-              <path d="M20.2,2H19.5H18C17.1,2 16,3 16,4H8C8,3 6.9,2 6,2H4.5H3.8H2V11C2,12 3,13 4,13H6.2C6.6,15 7.9,16.7 11,17V19.1C8.8,19.3 8,20.4 8,21.7V22H16V21.7C16,20.4 15.2,19.3 13,19.1V17C16.1,16.7 17.4,15 17.8,13H20C21,13 22,12 22,11V2H20.2M4,11V4H6V6V11C5.1,11 4.3,11 4,11M20,11C19.7,11 18.9,11 18,11V6V4H20V11Z"
-              />
-            </svg>
-          </div>
-          <h2>Some more swag</h2>
-        </div>
-      </main>
+<?php printsuccesses(); ?>
+              </main>
     </section>
     <div class="clearfix"></div>
 
