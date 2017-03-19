25a8e82b48898fce63f164c04f1cec7922ed2cbe
quannten24 <bendix.sonnenberg@gmx.de>
2017-03-19 21:41:27 +0100

    starting to integrate content

--- achievments/index.php
+++ achievments/index.php
@@ -27,6 +27,24 @@ function setHtmlLang() {
         echo "de-de";
     }
 }
+function printsuccess($suc){
+echo '        <div class="achievment-content">
+          <div class="trophy achievment-trophy">
+            <svg class="icon-big" viewBox="0 0 24 24">
+              <path d="M20.2,2H19.5H18C17.1,2 16,3 16,4H8C8,3 6.9,2 6,2H4.5H3.8H2V11C2,12 3,13 4,13H6.2C6.6,15 7.9,16.7 11,17V19.1C8.8,19.3 8,20.4 8,21.7V22H16V21.7C16,20.4 15.2,19.3 13,19.1V17C16.1,16.7 17.4,15 17.8,13H20C21,13 22,12 22,11V2H20.2M4,11V4H6V6V11C5.1,11 4.3,11 4,11M20,11C19.7,11 18.9,11 18,11V6V4H20V11Z"
+              />
+            </svg>
+          </div>
+          <h2>'. $suc -> name.'</h2>
+        </div>
+        <div class="achievment-content">
+          <div class="trophy achievment-trophy">
+            <svg class="icon-big" viewBox="0 0 24 24">
+              <path d="M20.2,2H19.5H18C17.1,2 16,3 16,4H8C8,3 6.9,2 6,2H4.5H3.8H2V11C2,12 3,13 4,13H6.2C6.6,15 7.9,16.7 11,17V19.1C8.8,19.3 8,20.4 8,21.7V22H16V21.7C16,20.4 15.2,19.3 13,19.1V17C16.1,16.7 17.4,15 17.8,13H20C21,13 22,12 22,11V2H20.2M4,11V4H6V6V11C5.1,11 4.3,11 4,11M20,11C19.7,11 18.9,11 18,11V6V4H20V11Z"
+              />
+            </svg>
+          </div>'
+}
 ?>
 
   <!DOCTYPE html>
@@ -115,27 +133,12 @@ function setHtmlLang() {
     </header>
 
     <main id="achievments">
-      <h1>DM 2017</h1>
+      <h1><?php echo $tournament[0] -> title ?></h1>
     </main>
 
     <section class="contest">
       <main class="inner-contest achievment-box">
-        <div class="achievment-content">
-          <div class="trophy achievment-trophy">
-            <svg class="icon-big" viewBox="0 0 24 24">
-              <path d="M20.2,2H19.5H18C17.1,2 16,3 16,4H8C8,3 6.9,2 6,2H4.5H3.8H2V11C2,12 3,13 4,13H6.2C6.6,15 7.9,16.7 11,17V19.1C8.8,19.3 8,20.4 8,21.7V22H16V21.7C16,20.4 15.2,19.3 13,19.1V17C16.1,16.7 17.4,15 17.8,13H20C21,13 22,12 22,11V2H20.2M4,11V4H6V6V11C5.1,11 4.3,11 4,11M20,11C19.7,11 18.9,11 18,11V6V4H20V11Z"
-              />
-            </svg>
-          </div>
-          <h2>Best car</h2>
-        </div>
-        <div class="achievment-content">
-          <div class="trophy achievment-trophy">
-            <svg class="icon-big" viewBox="0 0 24 24">
-              <path d="M20.2,2H19.5H18C17.1,2 16,3 16,4H8C8,3 6.9,2 6,2H4.5H3.8H2V11C2,12 3,13 4,13H6.2C6.6,15 7.9,16.7 11,17V19.1C8.8,19.3 8,20.4 8,21.7V22H16V21.7C16,20.4 15.2,19.3 13,19.1V17C16.1,16.7 17.4,15 17.8,13H20C21,13 22,12 22,11V2H20.2M4,11V4H6V6V11C5.1,11 4.3,11 4,11M20,11C19.7,11 18.9,11 18,11V6V4H20V11Z"
-              />
-            </svg>
-          </div>
+
           <h2>Coolest portfolie</h2>
         </div>
         <div class="achievment-content">
