7be206bbcf2a7680f3b3fcb9e7049e999ec2343f
quannten24 <bendix.sonnenberg@gmx.de>
2017-03-30 09:41:48 +0200

    only one entry for image and data these two should be the same in every language

--- webin/news/newsedit.php
+++ webin/news/newsedit.php
@@ -10,19 +10,19 @@ $newsen = json_decode(file_get_contents('../../content/news/newsen-en.json'));
 <html>
 <body>
 <form action="submit.php" method="POST">
+
+	<input name="image" type="text" value="<?php echo $newsde[$_GET['index']] -> image ?>"></input>	
+	<input name="date" type="text" value="<?php echo $newsde[$_GET['index']] -> date?>"></input>	
+<br>
 	<input name="detitle" type="text" value="<?php echo $newsde[$_GET['index']] -> title?>"></input>	
-	<input name="dedate" type="text" value="<?php echo $newsde[$_GET['index']] -> date?>"></input>	
 	<input name="desubtitle" type="text" value="<?php echo $newsde[$_GET['index']] -> subtitle?>"></input>	
 	<input name="depreview" type="text" value="<?php echo $newsde[$_GET['index']] -> preview?>"></input>	
-	<input name="deimage" type="text" value="<?php echo $newsde[$_GET['index']] -> image ?>"></input>	
 	<textarea name="detext"  value=""><?php echo $newsde[$_GET['index']] -> text?></textarea>	
 	<!-- TODO token -->
 <br>
 	<input name="entitle" type="text" value="<?php echo $newsen[$_GET['index']] -> title?>"></input>	
-	<input name="endate" type="text" value="<?php echo $newsen[$_GET['index']] -> date?>"></input>	
 	<input name="ensubtitle" type="text" value="<?php echo $newsen[$_GET['index']] -> subtitle?>"></input>	
 	<input name="enpreview" type="text" value="<?php echo $newsen[$_GET['index']] -> preview?>"></input>	
-	<input name="enimage" type="text" value="<?php echo $newsen[$_GET['index']] -> image?>"></input>	
 	<textarea name="entext"  value=""><?php echo $newsen[$_GET['index']] -> text?></textarea>	
 	<input type="hidden" name="index"value="<?php echo $_GET['index'] ?>"/>
 	<!-- TODO token -->
--- webin/news/submit.php
+++ webin/news/submit.php
@@ -6,30 +6,30 @@ $denewsfile = json_decode(file_get_contents("../../content/news/newsde-de.json")
 $ennewsfile = json_decode(file_get_contents("../../content/news/newsen-en.json"));
 if($_POST['index']>= 0){
 	$denewsfile[$_POST['index']] -> title = $_POST['detitle'];
-	$denewsfile[$_POST['index']] -> date = $_POST['dedate'];
+	$denewsfile[$_POST['index']] -> date = $_POST['date'];
 	$denewsfile[$_POST['index']] -> subtitle  = $_POST['desubtitle'];
 	$denewsfile[$_POST['index']] -> preview  = $_POST['depreview'];
-	$denewsfile[$_POST['index']] -> image   = $_POST['deimage'];
+	$denewsfile[$_POST['index']] -> image   = $_POST['image'];
 	$denewsfile[$_POST['index']] -> text   = $_POST['detext'];
 	$ennewsfile[$_POST['index']] -> title = $_POST['entitle'];
-	$ennewsfile[$_POST['index']] -> date = $_POST['endate'];
+	$ennewsfile[$_POST['index']] -> date = $_POST['date'];
 	$ennewsfile[$_POST['index']] -> subtitle  = $_POST['ensubtitle'];
 	$ennewsfile[$_POST['index']] -> preview  = $_POST['enpreview'];
-	$ennewsfile[$_POST['index']] -> image   = $_POST['enimage'];
+	$ennewsfile[$_POST['index']] -> image   = $_POST['image'];
 	$ennewsfile[$_POST['index']] -> text   = $_POST['entext'];
 }
 else{
 	$denews -> title = $_POST['detitle'];
-	$denews -> date = $_POST['dedate'];
+	$denews -> date = $_POST['date'];
 	$denews -> subtitle  = $_POST['desubtitle'];
 	$denews -> preview  = $_POST['depreview'];
-	$denews -> image   = $_POST['deimage'];
+	$denews -> image   = $_POST['image'];
 	$denews -> text   = $_POST['detext'];
 	$ennews -> title = $_POST['entitle'];
-	$ennews -> date = $_POST['endate'];
+	$ennews -> date = $_POST['date'];
 	$ennews -> subtitle  = $_POST['ensubtitle'];
 	$ennews -> preview  = $_POST['enpreview'];
-	$ennews -> image   = $_POST['enimage'];
+	$ennews -> image   = $_POST['image'];
 	$ennews -> text   = $_POST['entext'];
 	array_insert($denewsfile,$denews);
 	array_insert($ennewsfile,$ennews);
