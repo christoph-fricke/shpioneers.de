7cdf965391c2f9ec288238446f8ca7e4a40ba063
quannten24 <bendix.sonnenberg@gmx.de>
2017-03-30 15:19:27 +0200

    was only changing one file

--- webin/team/remove.php
+++ webin/team/remove.php
@@ -5,6 +5,6 @@ $deteam = json_decode(file_get_contents("../../content/team/team-de-de.json"));
 $enteam = json_decode(file_get_contents("../../content/team/team-en-en.json"));
 array_splice($deteam,$_GET['index'],1);
 array_splice($enteam,$_GET['index'],1);
-file_put_contents("../../content/team/team-en-en.json",json_encode($deteam));
+file_put_contents("../../content/team/team-de-de.json",json_encode($deteam));
 file_put_contents("../../content/team/team-en-en.json",json_encode($enteam));
 ?>
