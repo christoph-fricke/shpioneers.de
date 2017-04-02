cf8e4c1ca0816520b76e417a29bb4f6e908ed414
quannten24 <bendix.sonnenberg@gmx.de>
2017-04-02 16:29:17 +0200

    body for achievments

---
+++ webin/achievments/index.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+if(!($_SESSION['login'] === True)) die();
+function printachiev(){
+	
+}
+?>
+<!DOCTYPE>
+<html>
+<body>
+<a href="change.php?index=-1">Add Achievement</a>
+<?php preintachiev() ?>
+</body>
+</html>
