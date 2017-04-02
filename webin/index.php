a3952e11e99ab8d2a8dcbfcce6aab187840651c5
Christoph Fricke <christoph@frickeonline.de>
2017-04-02 20:16:07 +0200

    Styled login page and started to use sass

--- webin/index.php
+++ webin/index.php
@@ -1,10 +1,33 @@
 <!DOCTYPE>
 <html>
-<body>
-<form action="admin.php" method="POST">
-<input name="user" type="text" placeholder="Name"></input>
-<input name="password" type="password" placeholder="Password"></input>
-<input type="submit" value="Login"></input>
-</form>
-</body>
+    <head>
+        <title>Pioneers | Webinteface</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/assets/icons/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/assets/icons/favicons/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/assets/icons/favicons/manifest.json">
+        <link rel="mask-icon" href="/assets/icons/favicons/safari-pinned-tab.svg" color="#EA5B10">
+        <link rel="shortcut icon" href="/assets/icons/favicons/favicon.ico">
+        <meta name="msapplication-config" content="/assets/icons/favicons/browserconfig.xml">
+
+        <meta name="Author" content="Pioneers" />
+        <meta name="Description" content="" />
+        <meta name="Keywords" content="" />
+
+        <meta name="theme-color" content="#EA5B10" />
+        <meta name="format-detection" content="telephone=no" />
+
+        <link rel="stylesheet" href="../assets/css/webinterface/login.css" />
+        <link rel="stylesheet" href="../assets/css/materialdesignicons.min.css" />
+    </head>
+    <body class="page-container">
+            <form class="input-container" action="admin.php" method="POST">
+                <input class="input-item" name="user" type="text" placeholder="Name"></input>
+                <input class="input-item" name="password" type="password" placeholder="Password"></input>
+                <input class="input-item input-item--submit" type="submit" value="Login"></input>
+            </form>
+    </body>
 </html>
