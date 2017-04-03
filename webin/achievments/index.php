7b2c51a4e5ec00bcad56a26ceb1553147a152852
quannten24 <bendix.sonnenberg@gmx.de>
2017-04-03 11:41:13 +0200

    index for achievments and body for change.php

--- webin/achievments/index.php
+++ webin/achievments/index.php
@@ -2,13 +2,18 @@
 session_start();
 if(!($_SESSION['login'] === True)) die();
 function printachiev(){
-	
+	$list = json_decode(file_get_contents('../../content/achievments/list.json'));	
+	$i = 0;
+	foreach($list as $tour){
+		$tourname = json_decode(file_get_contents('../../content/achievments/'.$tour.'/en-en.json'));
+		echo '<a href="change.php?index='.$i.'">'.$tourname -> title.'</a>&nbsp<a href="remove.php?index='.$i++.'">Remove</a><br>';
+	}
 }
 ?>
 <!DOCTYPE>
 <html>
 <body>
-<a href="change.php?index=-1">Add Achievement</a>
-<?php preintachiev() ?>
+<a href="change.php?index=-1">Add Achievement</a><br>
+<?php printachiev() ?>
 </body>
 </html>
