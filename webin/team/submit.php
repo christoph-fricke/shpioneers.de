ade927b0fe1063c1b91d8024c060f85d62a9519b
quannten24 <bendix.sonnenberg@gmx.de>
2017-04-07 12:06:04 +0200

    the changing part of the submit.php is done

--- webin/team/submit.php
+++ webin/team/submit.php
@@ -1,46 +1,27 @@
 <?php
 session_start();
-if(!($_SESSION['login'] === True)||!checktoken()) die();
+if(!($_SESSION['login'] === True)||!checktoken()){
+	echo 'noc';
+ die();
+}
 $deteam = json_decode(file_get_contents("../../content/team/team-de-de.json"));
 $enteam = json_decode(file_get_contents("../../content/team/team-en-en.json"));
-if($_POST['index']>= 0){
-	$deteam[$_POST['index']] -> name = $_POST['name'];
-	$deteam[$_POST['index']] -> job = $_POST['dejob'];
-	$deteam[$_POST['index']] -> quote  = $_POST['dequote'];
-	$deteam[$_POST['index']] -> quotee  = $_POST['quotee'];
-	$deteam[$_POST['index']] -> text  = $_POST['detext'];
-	$deteam[$_POST['index']] -> img  = $_POST['img'];
-	//----------------------------------------------
-	$enteam[$_POST['index']] -> name = $_POST['name'];
-	$enteam[$_POST['index']] -> job = $_POST['enjob'];
-	$enteam[$_POST['index']] -> quote  = $_POST['enquote'];
-	$enteam[$_POST['index']] -> quotee  = $_POST['quotee'];
-	$enteam[$_POST['index']] -> text  = $_POST['entext'];
-	$enteam[$_POST['index']] -> img  = $_POST['img'];
-}
-else{
-	$demem -> name = $_POST['name'];
-	$demem -> job = $_POST['dejob'];
-	$demem -> quote  = $_POST['dequote'];
-	$demem -> quotee  = $_POST['quotee'];
-	$demem -> text  = $_POST['detext'];
-	$demem -> img  = $_POST['img'];
-	//----------------------------------------------
-	$enmem -> name = $_POST['name'];
-	$enmem -> job = $_POST['enjob'];
-	$enmem -> quote  = $_POST['enquote'];
-	$enmem -> quotee  = $_POST['quotee'];
-	$enmem -> text  = $_POST['entext'];
-	$enmem -> img  = $_POST['img'];
-	array_insert($deteam,$demem);
-	array_insert($enteam,$enmem);
-}
-if($_POST['index'] != $_POST['indexto'] && $_POST['index'] >= 0){
-	moveElement($deteam,$_POST['index'],$_POST['indexto']);
-	moveElement($enteam,$_POST['index'],$_POST['indexto']);
-}
+$deteam[$_POST['index']] -> name = $_POST['dename'];
+$deteam[$_POST['index']] -> job = $_POST['dejob'];
+$deteam[$_POST['index']] -> quote = $_POST['dequote'];
+$deteam[$_POST['index']] -> quotee = $_POST['dequotee'];
+$deteam[$_POST['index']] -> text = $_POST['detext'];
+$deteam[$_POST['index']] -> img = $_POST['deimg'];
+$enteam[$_POST['index']] -> name = $_POST['enname'];
+$enteam[$_POST['index']] -> job = $_POST['enjob'];
+$enteam[$_POST['index']] -> quote = $_POST['enquote'];
+$enteam[$_POST['index']] -> quotee = $_POST['enquotee'];
+$enteam[$_POST['index']] -> text = $_POST['entext'];
+$enteam[$_POST['index']] -> img = $_POST['enimg'];
+
 file_put_contents("../../content/team/team-de-de.json", json_encode($deteam));
 file_put_contents("../../content/team/team-en-en.json", json_encode($enteam));
+echo 'suc';
 function checktoken(){
 return $_POST['token'] == $_SESSION['teamtoken'];
 }
