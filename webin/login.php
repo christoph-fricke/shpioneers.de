aae18dacf913626a59c13ca6ba8445ae0b70ef5a
quannten24 <bendix.sonnenberg@gmx.de>
2017-04-11 17:43:33 +0200

    activated PasswordChecking

--- webin/login.php
+++ webin/login.php
@@ -1,11 +1,11 @@
 <?php
 function checkuser($name,$pass){
-/*	$userbase = json_decode(file_get_contents('users.json'));
+	$userbase = json_decode(file_get_contents('users.json'));
 	foreach($userbase as $user){
 		if($name == $user -> login && password_verify($pass,$user -> pass)) return True;
-	}*/ 
+	} 
 // The stuff above works but for debugging it is quite nice to not have to login in every so often
-	return True;
+	return false;
 }
 session_start();
 if(checkuser($_POST['user'],$_POST['password'])){
