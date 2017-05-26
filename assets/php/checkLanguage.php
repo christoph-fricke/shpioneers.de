25ad7ccf8a07d7344fffc9f796d29d011e4ac609
quannten24 <bendix.sonnenberg@gmx.de>
2017-05-26 14:35:25 +0200

    language will now stay even after reloading the page

--- assets/php/checkLanguage.php
+++ assets/php/checkLanguage.php
@@ -1,29 +1,31 @@
 <?php
 session_start();
-    if (isset($_GET['lang'])) {
-        $_SESSION['lang'] = $_GET['lang'];
-    }
-    else {
-        $_SESSION['lang'] = "en-en";
-    }
-    
+if (isset($_GET['lang'])) {
+	$_SESSION['lang'] = $_GET['lang'];
+}
+else {
+	if(!isset($_SESSION['lang'])){
+		$_SESSION['lang'] = "en-en";
+	}
+}
+
 switch ($_SESSION['lang']) {
-        case "en-en":
-            include_once('assets/lang/en-en.php');
-            break;
-        case "de-de":
-            include_once('assets/lang/de-de.php');
-            break;
-        default:
-            include_once('assets/lang/en-en.php');
-            break;
+	case "en-en":
+		include_once('assets/lang/en-en.php');
+		break;
+	case "de-de":
+		include_once('assets/lang/de-de.php');
+		break;
+	default:
+		include_once('assets/lang/en-en.php');
+		break;
 }
 
 function setHtmlLang() {
-    if ($_SESSION['lang']) {
-        echo $_SESSION['lang'];
-    } else {
-        echo "de-de";
-    }
+	if ($_SESSION['lang']) {
+		echo $_SESSION['lang'];
+	} else {
+		echo "de-de";
+	}
 }
 ?>
