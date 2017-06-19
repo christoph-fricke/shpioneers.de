d0eaa0c8b7b9231e72812e139bed29407ca81369
Christoph Fricke <christoph@frickeonline.de>
2017-06-19 19:24:43 +0200

    Added email validation

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -1,18 +1,26 @@
 $('#newsletter').submit(function (event) {
     event.preventDefault();
-
     var email = $('#newsletter__email').val();
-    var dataString = 'email=' + email + '&token=' + token;
 
-    var xhttp = $.ajax({
-        url: '/assets/php/newsletter/addSubscriber.php',
-        type: 'post',
-        data: dataString
-    });
+    if (email != "" && validateEmail(email)) {
+        var dataString = 'email=' + email + '&token=' + token;
 
-    xhttp.done(function (response, textStatus, jqXHR) {
-        response = response.split(';');
-        token = response[1];
-        console.log(response);
-    });
+        var xhttp = $.ajax({
+            url: '/assets/php/newsletter/addSubscriber.php',
+            type: 'post',
+            data: dataString
+        });
+
+
+        xhttp.done(function (response, textStatus, jqXHR) {
+            response = response.split(';');
+            token = response[1];
+            console.log(response[0]);
+        });
+    }
 });
+
+function validateEmail(email) {
+    var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return pattern.test(email);
+}
