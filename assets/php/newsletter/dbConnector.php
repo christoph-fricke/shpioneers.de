2371cdc3dede9f48aa4f20f9fbab475d80c22c0e
Christoph Fricke <christoph@frickeonline.de>
2017-06-19 23:01:25 +0200

    Fixed Connector

--- assets/php/newsletter/dbConnector.php
+++ assets/php/newsletter/dbConnector.php
@@ -5,7 +5,7 @@ try {
     $pdo = new PDO(DSN, USER, PASS);
     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "CREATE TABLE IF NOT EXISTS subscribers (date datetime, email text, hash text(64) PRIMARY KEY); CREATE TABLE IF NOT EXISTS pendingSubscribers (date datetime, email text PRIMARY KEY, hash text(64) PRIMARY KEY)";
+    $sql = "CREATE TABLE IF NOT EXISTS subscribers (date datetime, email text, hash text); CREATE TABLE IF NOT EXISTS pendingSubscribers (date datetime, email text, hash text)";
     $pdo -> exec($sql);
 }
 catch(PDOExeption $e) {
