0389197b8f2fe6c75708f48ce51b188b57d028a8
Christoph Fricke <christoph@frickeonline.de>
2017-06-19 22:34:06 +0200

    Create table pendingSubscribers if not exits

--- assets/php/newsletter/dbConnector.php
+++ assets/php/newsletter/dbConnector.php
@@ -5,7 +5,7 @@ try {
     $pdo = new PDO(DSN, USER, PASS);
     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "CREATE TABLE IF NOT EXISTS subscribers (id int PRIMARY KEY AUTO_INCREMENT, date datetime, email text, hash text, UNIQUE (email(255), hash(255)) )";
+    $sql = "CREATE TABLE IF NOT EXISTS subscribers (date datetime, email text, hash text PRIMARY KEY); CREATE TABLE IF NOT EXISTS pendingSubscribers (date datetime, email text PRIMARY KEY, hash text PRIMARY KEY)";
     $pdo -> exec($sql);
 }
 catch(PDOExeption $e) {
