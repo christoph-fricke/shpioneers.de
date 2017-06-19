f4d000a160a41cf8bc452f49e8208fb177e6f001
Christoph Fricke <christoph@frickeonline.de>
2017-06-19 16:11:55 +0200

    Fixed SQL syntax... Looks like there are a few differences between MySQL and SQLite...

--- assets/php/newsletter/dbConnector.php
+++ assets/php/newsletter/dbConnector.php
@@ -5,9 +5,10 @@ try {
     $pdo = new PDO(DSN, USER, PASS);
     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "CREATE TABLE IF NOT EXISTS subscribers (id integer PRIMARY KEY AUTOINCREMENT, email TEXT, hash TEXT)";
+    $sql = "CREATE TABLE IF NOT EXISTS subscribers (id int PRIMARY KEY AUTO_INCREMENT, email text, hash text)";
     $pdo -> exec($sql);
-} catch(PDOExeption $e) {
-    echo "Connection failed: " . $e -> getMessage();
+}
+catch(PDOExeption $e) {
+    echo 'Connection failed: ' . $e -> getMessage();
 }
 ?>
