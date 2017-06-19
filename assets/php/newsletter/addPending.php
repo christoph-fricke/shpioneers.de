68d01030a990488f59c89040c8a1b74d94808568
quannten24 <bendix.sonnenberg@gmx.de>
2017-06-19 22:34:01 +0200

    created file

---
+++ assets/php/newsletter/addPending.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+require_once('../../../admin/passwordLib.php');
+//Checks if the script is called with a valid token and if an email adress is provided.
+if ((!isset($_SESSION['token']) || $_SESSION['token'] != $_POST['token']) && !isset($_POST['email'])) {
+    die('No token and/or email is provided.');
+}
+
+require_once('dbConnector.php');
+
+$email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+$secure = true;
+$hash =	password_hash($email,PASSWORD_DEFAULT); 
+
+try {
+    $sql = "INSERT INTO pending_Subscribers ( email, hash, date) VALUES (:email, :hash, NOW())";
+    $prepared = $pdo -> prepare($sql);
+    $prepared -> execute(array('email' => $email, 'hash' => $hash));
+
+    $status = 1;
+}
+catch(Exception $e) {
+    $status = 0;
+}
+
+//Return feedback to the client
+echo $status . ';' . newToken();
+
+function newToken() {
+    $length = 32;
+    $secure = true;
+    
+    $newToken = bin2hex(openssl_random_pseudo_bytes($length, $secure));
+    $_SESSION['token'] = $newToken;
+    return $newToken;
+}
+?>
