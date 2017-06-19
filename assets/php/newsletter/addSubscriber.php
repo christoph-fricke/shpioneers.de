60a4a149df8184e9cd821fc771839fee0f1466cb
Christoph Fricke <christoph@frickeonline.de>
2017-06-19 23:45:37 +0200

    Some more changes

--- assets/php/newsletter/addSubscriber.php
+++ assets/php/newsletter/addSubscriber.php
@@ -4,9 +4,15 @@ require_once('../../../admin/passwordLib.php');
 //Checks if the script is called with a valid token and if an email adress is provided.
 require_once('dbConnector.php');
 
+$hash = $_GET['hash'];
+
 try {
-    $sql = "INSERT INTO subscribers (email, hash, date) SELECT email, hash, date FROM pendingSubscribers WHERE hash = \"{$_GET['hash']}\"";
-    $pdo -> exec($sql);
+    $sqlMove = "INSERT INTO subscribers (email, hash, date) SELECT email, hash, date FROM pendingSubscribers WHERE hash = \"{$hash}\"";
+    $pdo -> exec($sqlMove);
+    $sqlDelete = "DELETE FROM pendingSubscribers WHERE hash = \"{$hash}\"";
+    $pdo -> exec($sqlDelete);
+    // echo $sqlMove;
+    // echo $sqlDelete;
     $status = 1;
 }
 catch(Exception $e) {
--- assets/php/newsletter/dbConnector.php
+++ assets/php/newsletter/dbConnector.php
@@ -5,7 +5,7 @@ try {
     $pdo = new PDO(DSN, USER, PASS);
     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "CREATE TABLE IF NOT EXISTS subscribers (date datetime, email text, hash text); CREATE TABLE IF NOT EXISTS pendingSubscribers (date datetime, email text, hash text)";
+    $sql = "CREATE TABLE IF NOT EXISTS subscribers (date datetime, email text, hash text, UNIQUE(email(255))); CREATE TABLE IF NOT EXISTS pendingSubscribers (date datetime, email text, hash text, UNIQUE(email(255)))";
     $pdo -> exec($sql);
 }
 catch(PDOExeption $e) {
