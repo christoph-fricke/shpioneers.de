65ba69875f2b991e55e1bc65007b65c2a180f01e
Christoph Fricke <christoph@frickeonline.de>
2017-06-19 22:21:50 +0200

    Added datetime stamp to the database

--- assets/php/newsletter/addSubscriber.php
+++ assets/php/newsletter/addSubscriber.php
@@ -13,7 +13,7 @@ $secure = true;
 $hash =	password_hash($email,PASSWORD_DEFAULT); 
 
 try {
-    $sql = "INSERT INTO subscribers ( email, hash) VALUES (:email, :hash)";
+    $sql = "INSERT INTO subscribers ( email, hash, date) VALUES (:email, :hash, NOW())";
     $prepared = $pdo -> prepare($sql);
     $prepared -> execute(array('email' => $email, 'hash' => $hash));
 
