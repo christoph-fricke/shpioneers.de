a9f402cbe34ee4ef013e6f41a5210da84e52bb00
Christoph Fricke <christoph@frickeonline.de>
2017-06-20 14:53:31 +0200

    Everything should now be functional and sql injection immune

--- assets/php/newsletter/addSubscriber.php
+++ assets/php/newsletter/addSubscriber.php
@@ -3,15 +3,24 @@ require_once('../../../admin/passwordLib.php');
 require_once('dbConnector.php');
 
 $hash = $_GET['hash'];
+$email = "";
 echo "I ama here" . $hash;
 try {
     $sqlMove = "INSERT INTO subscribers (email, hash, date) SELECT email, hash, date FROM pendingSubscribers WHERE hash = :hash";
     $prepared = $pdo -> prepare($sqlMove);
     $prepared -> execute(array('hash' => $hash));
-    // $sqlGetEmail = "SELECT email FROM pendingSubscribers WHERE hash = \"{$hash}\"";
-    // $statement = $pdo -> query($sqlGetEmail);
-    // $response = $statement -> fetch(PDO::FETCH_ASSOC);
-    // echo $response;
+
+    //Gets the email of the 
+    $sqlGetEmail = "SELECT email FROM pendingSubscribers WHERE hash = :hash";
+    $prepared = $pdo -> prepare($sqlGetEmail);
+    $prepared -> execute(array('hash' => $hash));
+    $email = $prepared -> fetch(PDO::FETCH_ASSOC)['email'];
+
+    //Deletes all pending email addresses which are equal with the one in the database
+    $sqlDelete = "DELETE FROM pendingSubscribers WHERE email = :email";
+ 	$prepared = $pdo -> prepare($sqlDelete);
+    $prepared -> execute(array('email' => $email));
+    
     $status = 1;
 }
 catch(Exception $e) {
