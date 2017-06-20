0b46bc0243f25f6cfc54067c65687d40366f0804
quannten24 <bendix.sonnenberg@gmx.de>
2017-06-20 20:29:02 +0200

    created a basic confirm.php file also changed the address in (sendConfirmation.php)[sendConfirmation.php] to localhost

--- assets/php/newsletter/sendConfirmation.php
+++ assets/php/newsletter/sendConfirmation.php
@@ -14,14 +14,14 @@ switch($lang){
 $from = "FROM: info@shpioneers.de; Content-Type: text/html; charset=UTF-8";
 $to = $email;
 $subject = CONFIRM_SUB;  
-$message = MESSAGE_FRONT. "<a href=\"http://shpioneers.de/confirm.php?hash={$hash}\">".MESSAGE_MIDDLE."</a>". MESSAGE_BACK;
+$message = MESSAGE_FRONT. "<a href=\"http://localhost/confirm.php?hash={$hash}\">".MESSAGE_MIDDLE."</a>". MESSAGE_BACK;
  try {
         mail($to, $subject, $message, $from);
         $status = 1;
     } catch(Exeption $e) {
         $status = 0;
     }
-
+echo $message;
 
 
 ?>
