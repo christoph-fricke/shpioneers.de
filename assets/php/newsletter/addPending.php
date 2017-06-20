8965b4ec9c8839c4fe7df92ead2b4cab080b9382
quannten24 <bendix.sonnenberg@gmx.de>
2017-06-20 23:18:46 +0200

    removed debug readouts

--- assets/php/newsletter/addPending.php
+++ assets/php/newsletter/addPending.php
@@ -9,7 +9,6 @@ if ((!isset($_SESSION['token']) || $_SESSION['token'] != $_POST['token']) && !is
 require_once('dbConnector.php');
 
 $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
-echo $email;
 $hash =	password_hash($email,PASSWORD_DEFAULT); 
 $lang = 1;// TODO somehow include the language
 try {
