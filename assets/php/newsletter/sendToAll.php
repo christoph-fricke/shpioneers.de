7fab30b168ecc8ab4f7587846a9b2ac79dfd0eae
quannten24 <bendix.sonnenberg@gmx.de>
2017-06-21 22:24:59 +0200

    unsubscribe link at bottom of the page

--- assets/php/newsletter/sendToAll.php
+++ assets/php/newsletter/sendToAll.php
@@ -1,5 +1,9 @@
 <?php
 include('dbConnector.php');
+define(UNSUBSCRIBEDE1,'<br> Zum abmelden von diesem Newsletter <a href="shpioneers.de/unsubscribe.php?hash=');
+define(UNSUBSCRIBEDE2,'">hier</a> klicken.');
+define(UNSUBSCRIBEEN1,'<br>To unsubscribe click <a href="shpioneers.de/unsubscribe.php?hash=');
+define(UNSUBSCRIBEEN2,'">here</a>.');
 // TODO authentication
 $sql = "SELECT email,hash,lang FROM subscribers";
 try{
@@ -15,16 +19,20 @@ function sendEmailTo($to,$hash,$lang){
 switch($lang){
 	case 0:
 		$lng = "de";
+		$unsub = UNSUBSCRIBEDE1 . $hash. UNSUBSCRIBEDE2;
 		break;
 	case 1:
 		$lng = "en";
+		$unsub = UNSUBSCRIBEEN1 . $hash. UNSUBSCRIBEEN2;
 		break;
 	default:
 		$lng = "de";
+		$unsub = UNSUBSCRIBEDE1 . $hash. UNSUBSCRIBEDE2;
 		break;
 }
 $header = "FROM: info@shpioneers.de\n";
 $header .= "Content-Type: text/html; charset= UTF-8\n";
-mail($to,$_POST["subject{$lng}"],$_POST["message{$lng}"],$header);
+$message = $_POST["message{$lng}"] . $unsub;
+mail($to,$_POST["subject{$lng}"],$message,$header);
 }
 ?>
