33368327f5688f0c03e65f509cb4468717e67823
quannten24 <bendix.sonnenberg@gmx.de>
2017-07-06 14:14:01 +0200

    description for content

--- assets/php/newsletter/addEmail.php
+++ assets/php/newsletter/addEmail.php
@@ -1,4 +1,10 @@
 <?php
+/*
+Only upload this file to the server if you have the permission of the persons to which you will send the emails.
+otherwise they might sue you.
+also delete this file after executing it, because it could screw up your db if you are not carefull.
+also do not input an email adress that already is in the db, because all other commands after that will be ignored
+*/
 include('dbConnector.php');
 require_once('../../../admin/passwordLib.php');
 function addSub($email){
